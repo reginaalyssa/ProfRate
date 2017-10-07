e7722a0d7e569b992dd9b54544ff13fec8ef1cda
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-10-07 21:32:47 +0800

    Add validation for professor first_name&last_name fields and for course title field

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :professors
+  validates :title, presence: true
 end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,5 +1,7 @@
 class Professor < ApplicationRecord
   has_and_belongs_to_many :courses
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
