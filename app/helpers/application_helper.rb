c972a359910fa725c5ca6a84a8f980cd51f05a41
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-10-08 22:47:53 +0800

    Use partials in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "ProfRate - Rate your professors"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
